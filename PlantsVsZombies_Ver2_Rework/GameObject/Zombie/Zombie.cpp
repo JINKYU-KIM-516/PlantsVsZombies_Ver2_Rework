6b70ecb80eb9cab39903f3ae19b32d4628feace7
JINKYU-KIM-516 <qlika0516@naver.com>
2025-08-12 11:47:44 +0900

    Zombie에 HP Component 추가

--- PlantsVsZombies_Ver2_Rework/GameObject/Zombie/Zombie.cpp
+++ PlantsVsZombies_Ver2_Rework/GameObject/Zombie/Zombie.cpp
@@ -1,9 +1,5 @@
 #include "Zombie.h"
 
-void Zombie::CheckAlive()
-{
-}
-
 Zombie::Zombie(Point* p_pos)
 {
 	Init(p_pos);
@@ -19,9 +15,8 @@ void Zombie::Init(Point* p_pos)
 {
 	m_pos = p_pos;
 	m_size = new ZOMBIE_SIZE;
-	m_image = new Image(m_pos, m_size, ZOMBIE_IMAGEPATH);
-	m_move = new Move(m_pos, Direction::WEST, ZOMBIE_MOVESPEED);
 
-	AddComponent(m_image);
-	AddComponent(m_move);
+	AddComponent(new Image(m_pos, m_size, ZOMBIE_IMAGEPATH));
+	AddComponent(new Move(m_pos, Direction::WEST, ZOMBIE_MOVESPEED));
+	AddComponent(new HealthPointComponent(ZOMBIE_HEALTHPOINT));
 }
--- PlantsVsZombies_Ver2_Rework/GameObject/Zombie/Zombie.h
+++ PlantsVsZombies_Ver2_Rework/GameObject/Zombie/Zombie.h
@@ -2,19 +2,16 @@
 #include "../GameObject.h"
 #include "../../Component/Image.h"
 #include "../../Component/Move.h"
+#include "../../Component/HealthPointComponent.h"
 
 class Zombie : public GameObject
 {
 protected:
 	Point* m_pos;
 	Size* m_size;
-	Image* m_image;
-	Move* m_move;
-
 
 public:
 	Zombie(Point* p_pos);
 	~Zombie();
 	void Init(Point* p_pos);
-	void Update();
 };
